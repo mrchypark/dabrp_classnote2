931ac0aa9019c36e91e310273b2d42ecc3104094
samlee <leesaem218@gmail.com>
2017-07-01 14:29:49 +0900

    homework_week2

---
+++ run_sam.R
@@ -0,0 +1,21 @@
+## DBI
+
+library(DBI)
+library(RSQLite)
+con <- dbConnect(RSQLite::SQLite(), dbname="sql_sam.db")
+ls()
+nycflights13::airlines
+
+# check db tables
+dbListTables(con)
+
+
+# write table to db
+dbWriteTable(con, "airlines", nycflights13::airlines, overwrite=T)
+dbWriteTable(con, "aiports", nycflights13::airports, overwrite=T)
+dbWriteTable(con, "flights", nycflights13::flights, overwrite=T)
+dbWriteTable(con, "planes", nycflights13::planes, overwrite=T)
+dbWriteTable(con, "weather", nycflights13::weather, overwrite=T)
+
+dbListTables(con)
+
