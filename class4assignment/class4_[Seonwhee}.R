d81146dc832817153be64230353c2ce746f77f25
Seonwhee-Genome <quarklep@naver.com>
2017-07-15 16:16:59 +0900

    Problem 1 solved

--- class4assignment/class4_[Seonwhee}.R
+++ class4assignment/class4_[Seonwhee}.R
@@ -3,7 +3,12 @@ if (!require(data.table)) install.packages("data.table")
 ITEM <-fread("./recomen/item.csv")
 head(ITEM)
 LIP = grep("*립$", ITEM$cate_3_name)
-Answer1_total = ITEM[LIP,]
+Answer1_Lib = ITEM[LIP,]
+
+Sauce = grep("*(소스)$", ITEM$cate_3_name)
+Answer1_Sauce = ITEM[Sauce,]
+
+
 Answer1 = Answer1_total$partner
 Answer1
 ##################
