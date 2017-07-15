a0630e620b6160134f29d8037f7831f4a148bc1a
Seonwhee-Genome <quarklep@naver.com>
2017-07-15 23:32:05 +0900

    Homework Problem1 solved

--- class4assignment/class4_[Seonwhee}.R
+++ class4assignment/class4_[Seonwhee}.R
@@ -3,13 +3,20 @@ if (!require(data.table)) install.packages("data.table")
 ITEM <-fread("./recomen/item.csv")
 head(ITEM)
 LIP = grep("*립$", ITEM$cate_3_name)
-Answer1_Lib = ITEM[LIP,]
+Answer1_Lip = ITEM[LIP,]
 
 Sauce = grep("*(소스)$", ITEM$cate_3_name)
 Answer1_Sauce = ITEM[Sauce,]
 
+Slash = grep("*//*", ITEM$cate_3_name)
+Products = ITEM$cate_3_name[Slash]
+Products_list = strsplit(Products, "/")
+Products_vector = unlist(Products_list)
+Answer1_Slash = unique(Products_vector)
 
-Answer1 = Answer1_total$partner
-Answer1
+Answer1_1 = Answer1_Lip$partner
+Answer1_2 = Answer1_Sauce$cate_3_name
+Answer1_3 = length(Answer1_Slash)
+
 ##################
 
